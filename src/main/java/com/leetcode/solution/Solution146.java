17c3a114bf3d4babff6a6c1d57788406e8bce385
yangguang14 <yangguang14@58.com>
2020-05-25 18:59:40 +0800

    DEV:146 LRUCache

---
+++ src/main/java/com/leetcode/solution/Solution146.java
@@ -0,0 +1,24 @@
+package com.leetcode.solution;
+
+import com.leetcode.solution.structure.LRUCache;
+
+/**
+ * LRU缓存机制
+ * https://leetcode-cn.com/problems/lru-cache/
+ *
+ * @author yangguang14
+ * @date 2020/5/25.
+ */
+public class Solution146 {
+
+
+    public static void main(String[] args) {
+        int capacity = 10;
+        LRUCache obj = new LRUCache(capacity);
+        int key = 1;
+        int value = 1000;
+        System.out.println(obj.get(key));
+        obj.put(key, value);
+        System.out.println(obj.get(key));
+    }
+}
