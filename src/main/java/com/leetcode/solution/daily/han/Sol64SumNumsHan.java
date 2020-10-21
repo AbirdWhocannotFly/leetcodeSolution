3fc154a8628068b56183a7d431b4cb80f1f37460
Broccoli-han <Hasonlong@163.com>
2020-10-21 11:05:22 +0800

    删除person实体

--- src/main/java/com/leetcode/solution/daily/han/Sol64SumNumsHan.java
+++ src/main/java/com/leetcode/solution/daily/han/Sol64SumNumsHan.java
@@ -1,11 +1,5 @@
 package com.leetcode.solution.daily.han;
 
-import com.leetcode.solution.daily.han.entity.Person;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -60,15 +54,15 @@ public class Sol64SumNumsHan {
 
         String  reg = "^(([0]+\\.[0-9]*[1-9][0-9]*)|([1-9]*[1-9][0-9]*\\.[0-9]+)|([1-9]*[1-9][0-9]*))$";
         Pattern p=Pattern.compile(reg);
-         Matcher m = p.matcher("0.1");
-        Matcher m1 = p.matcher("0");
-        Matcher m2 = p.matcher("001");
-        Matcher m3 = p.matcher("1044.8");
-
-        System.out.println(m.find());
-        System.out.println(m1.find());
-        System.out.println(m2.find());
-        System.out.println(m3.find());
+//         Matcher m = p.matcher("0.1");
+//        Matcher m1 = p.matcher("0");
+//        Matcher m2 = p.matcher("001");
+//        Matcher m3 = p.matcher("1044.8");
+//
+//        System.out.println(m.find());
+//        System.out.println(m1.find());
+//        System.out.println(m2.find());
+//        System.out.println(m3.find());
 
 //        String s = "2020/07/02";
 //        String s1 = "2020-07-02";
